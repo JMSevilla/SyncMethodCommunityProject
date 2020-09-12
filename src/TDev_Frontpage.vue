4138a773e859bccc9524b1394375a8d2042aa500
migz123 <44269169+migz123@users.noreply.github.com>
2020-09-13 01:07:25 +0800

    Initial commit

---
+++ src/TDev_Frontpage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <!-- JM Contribute start (9/12/2020) -->
+    <contentdesign/>
+    <div class="img-wrapper">
+      <img src="./assets/large_syncmethod.png" alt="logo" width="20%">
+    </div>
+    <h1>Core Team starts here.</h1>
+
+    <!-- JM Contribute end (9/12/2020) -->
+  </div>
+</template>
+
+<script>
+import contentdesign from "./components/Content.vue";
+export default {
+  name: "TDev_Frontpage",
+  components: {
+    contentdesign
+  }
+};
+</script>
+//style contribute by: Klarissa (9/13/2020)
+<style scoped>
+.wrapper {
+  height: 100%;
+  /* background-color: lightblue; */
+}
+.img-wrapper {
+  display: flex;
+  background-color: gray;
+  margin: 2rem auto;
+  width: 85%;
+  height: 30%;
+  justify-content: flex-start;
+  align-items: flex-end;
+}
+</style>
+//style contribute by: Klarissa (9/13/2020)
+
---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import App from "./App.vue";
+import vuerouter from "vue-router";
+import frontpage from "./TDev_Frontpage.vue";
+import BootstrapVue from "bootstrap-vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import axios from "axios";
+const base = axios.create({
+  baseURL: "https://localhost:44332/"
+});
+Vue.prototype.$http = base;
+Vue.use(VueSweetalert2);
+Vue.use(BootstrapVue);
+Vue.use(vuerouter);
+Vue.use(Vuetify);
+Vue.config.productionTip = false;
+
+const router = new vuerouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "fronttestdev",
+      component: frontpage
+    }
+  ]
+});
+new Vue({
+  router,
+  render: (h) => h(App)
+}).$mount("#app");
